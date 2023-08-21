7e3ddc91bdfee2a68f611ad769743035a10e03cd
mohammdowais <mohdowais1920@gmail.com>
2023-08-22 00:36:37 +0530

    first messages

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(session({
+  secret:'mysecretkey',
+  saveUninitialized: true,
+  resave: true,
+}));
+
+app.use(flash());
+
+
+app.get('/', function(req, res){
+  const message = req.flash('message')
+  res.render('index', { message: message });
+});
+
+app.get('/success-flash', function(req, res){
+  req.flash('message', ['User added successfully!','success'])
+  res.redirect('/');
+  console.log(req.session)
+});
+
+app.get('/no-flash', function(req, res){
+  res.redirect('/');
+});
+
+app.get('/error-flash', function(req, res){
+    req.flash('message', ['some error occured', 'danger']);
+    res.redirect('/');
+});
+
+app.listen(3000,_=>console.log("server started"));
